326d777dffff10537a848350f9cf8209ab62d43f
adityarocky <96563791+adityarocky@users.noreply.github.com>
2022-03-03 10:19:04 +0530

    #include<stdio.h> int main() {     int size;     scanf("%d",&size);     int x[size],i;     for(i=0;i<size;i++)     scanf("%d",&x[i]);     for(i=0;i<size;i++)     {         if((x[i]%2!=0)&&(i%2==0))         {             printf("False");             return 0;         }     }     printf("True"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int size;
+    scanf("%d",&size);
+    int x[size],i;
+    for(i=0;i<size;i++)
+    scanf("%d",&x[i]);
+    for(i=0;i<size;i++)
+    {
+        if((x[i]%2!=0)&&(i%2==0))
+        {
+            printf("False");
+            return 0;
+        }
+    }
+    printf("True");
+}
